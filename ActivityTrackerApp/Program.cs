72ed305495b532277c2b4c974b886142dbc294d4
thgit <thgit@protonmail.com>
2022-06-08 13:50:12 -0500

    Add nLog

--- ActivityTrackerApp/Program.cs
+++ ActivityTrackerApp/Program.cs
@@ -1,6 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using NLog;
+using NLog.Web;
 
-app.MapGet("/", () => "Hello World!");
+var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init main");
 
-app.Run();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    var app = builder.Build();
+
+    app.MapGet("/", () => "Hello World!");
+
+    app.Run();
+}
+catch (Exception exception)
+{
+    // NLog: catch setup errors
+    logger.Error(exception, "Stopped program because of exception");
+    throw;
+}
+finally
+{
+    // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+    NLog.LogManager.Shutdown();
+}
